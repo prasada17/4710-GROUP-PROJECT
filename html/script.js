8397af37648633ad6dfd260ff4e1b0ec6aedc62d
macklellan <mcclellanr15@students.ecu.edu>
2019-03-17 02:59:41 -0400

    TEMPLATE IMPLEMENTED FURTHER
    - Added modal for Upload requirement.
    - Tabbing is fully implemented.
    - Added demo table.
    - Added demo table view after mock upload.
    - Various JS funtions for responsive template.
    - More CSS work.
    - Implemented responsive continue button
    
    I will comment the code better tomorrow.

--- html/script.js
+++ html/script.js
@@ -1,5 +1,38 @@
 
-function activecrumb() {
-    var element = document.getElementById("myDIV");
-    element.classList.add("mystyle");
+
+// Hides tab below the given number, unhides tab of the given number.
+function switchtab(number) {
+    let element = document.getElementById("tab-container" + (number-1).toString());
+    let element2 = document.getElementById("t" + (number-1).toString());
+    element.classList.add("hidden");
+    element2.classList.remove("active");
+
+    element = document.getElementById("tab-container" + number.toString());
+    element2 = document.getElementById("t" + number.toString());
+    element.classList.remove("hidden");
+    element2.classList.add("active");
+
+}
+
+
+function loading() {
+    let element = document.getElementById("load");
+    element.classList.remove("hidden");
+}
+
+function unload() {
+    let element = document.getElementById("load");
+    element.classList.add("hidden");
+}
+
+function showtable() {
+    let element = document.getElementById("filedrop");
+    element.innerHTML = document.getElementById("filebutton").innerHTML;
+}
+
+function contbutton() {
+    let element = document.getElementById("continuebutton");
+    element.classList.remove("inactive");
+    element.classList.add("active");
+    element.innerHTML = "<h3 id=\"test2\">Continue</h3>";
 }
