18be17eb10b7f34e3f77be243f4f6ec00c5616e7
macklellan <mcclellanr15@students.ecu.edu>
2019-03-17 23:59:49 -0400

    TAB LINKING PART 1 - switch tabs now changes active state of breadcrumb bar elements(upload, verify, ...)(script.js) - class continuebutton added for continue button with (active/passive) states(style.css/hydro.html) - Linked tab#0, tab#1, and tab#2

--- html/script.js
+++ html/script.js
@@ -4,10 +4,13 @@
 function switchtab(number) {
     for (let i = 0; i < 5; i++) {
         let element = document.getElementById("tab-container" + i.toString());
+        let element2 = document.getElementById("t" + i.toString());
         if (number == i) {
             element.classList.remove("hidden");
+            element2.classList.add("active");
         } else {
             element.classList.add("hidden");
+            element2.classList.remove("active");
         }
     }
 }
@@ -33,8 +36,8 @@ function showdatamodtable() {
     element.innerHTML = document.getElementById("datamodtable").innerHTML;
 }
 
-function contbutton() {
-    let element = document.getElementById("continuebutton");
+function contbutton(number) {
+    let element = document.getElementById("continuebutton" + number.toString());
     element.classList.remove("inactive");
     element.classList.add("active");
     element.innerHTML = "<h3 id=\"test2\">Continue</h3>";
