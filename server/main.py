2a78a35b4f0638e9e6518d2ab0407839a012112c
macklellan <mcclellanr15@students.ecu.edu>
2019-03-30 23:37:11 -0400

    Added Flask sever and initial configuration...

---
+++ server/main.py
@@ -0,0 +1,50 @@
+
+import os
+from flask import Flask, request, render_template, redirect, url_for
+from werkzeug.utils import secure_filename
+import util
+
+# get current app directory
+dir_path = os.path.dirname(os.path.realpath(__file__))
+UPLOAD_FOLDER = dir_path + '/data/'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/api/post_csv', methods=['POST'])
+def post_csv():
+	if request.method == 'POST':
+		# request.file <class 'werkzeug.datastructures.FileStorage'>
+		# request.url is http://127.0.0.1:5000/api/post_csv
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			log = 'no file field in request.'
+			return render_template('fail.html', log = log)
+		# print(request.files['file'])
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			log = 'Empty filename.'
+			return render_template('fail.html', log = log)
+		if file and util.allowed_file(file.filename):
+			# get filename in a safe way
+			filename = secure_filename(file.filename)
+			# check if the data folder exists, if not create one
+			if os.path.exists(app.config['UPLOAD_FOLDER']) == False:
+				os.makedirs(app.config['UPLOAD_FOLDER'])
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return render_template('success.html',filename='filename')
+
+@app.route('/')
+def index():
+    # this is your index page
+    log = 'Hello world.'
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    ip = '127.0.0.1'
+    app.run(host=ip)
+
---
+++ server/util.py
@@ -0,0 +1,6 @@
+# if a file extension is not listed, the system will not upload the file
+ALLOWED_EXTENSIONS = set(['csv'])
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
