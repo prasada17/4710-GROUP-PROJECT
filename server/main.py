bb41fc36bbb55233f1c17bd3b21dcfce66c156e7
macklellan <mcclellanr15@students.ecu.edu>
2019-04-02 13:24:30 -0400

    - CSV PREVIEW WORK  - Can preview no problem using example 38 code.. Dropzone is interfeering with returning a new template.... Need help

--- server/main.py
+++ server/main.py
@@ -1,5 +1,6 @@
 
 import os
+import time
 from flask import Flask, request, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 from flask_dropzone import Dropzone
@@ -12,41 +13,36 @@ UPLOAD_FOLDER = dir_path + '/data/'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/api/post_csv', methods=['POST'])
-def post_csv():
-	# request.file <class 'werkzeug.datastructures.FileStorage'>
-	# request.url is http://127.0.0.1:5000/api/post_csv
-	# check if the post request has the file part
-	if 'file' not in request.files:
-		log = 'no file field in request.'
-		return render_template('fail.html', log = log)
-	# print(request.files['file'])
-	file = request.files['file']
-	# if user does not select file, browser also
-	# submit an empty part without filename
-	if file.filename == '':
-		log = 'Empty filename.'
-		return render_template('fail.html', log = log)
-	if file and util.allowed_file(file.filename):
-		# get filename in a safe way
-		# filename = secure_filename(file.filename)
-		filename = file.filename
-		# check if the data folder exists, if not create one
-		if os.path.exists(app.config['UPLOAD_FOLDER']) == False:
-			os.makedirs(app.config['UPLOAD_FOLDER'])
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-	# 	# DROPZONE_REDIRECT_VIEW = ('upload2.html')
-	# 	print('uploading')
-	return render_template('upload.html')
-
-
-@app.route('/')
-def index():
-    return render_template('upload.html')
+@app.route('/' , methods=['POST', 'GET'])
+def upload():
+	if request.method == 'POST':
+		# request.file <class 'werkzeug.datastructures.FileStorage'>
+		# request.url is http://127.0.0.1:5000/
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			log = 'no file field in request.'
+			return render_template('fail.html', log = log)
+		# print(request.files['file'])
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			# This part should use flash to output information
+			log = 'Empty filename.'
+			return render_template('fail.html', log = log)
+		if file and util.allowed_file(file.filename):
+			# get filename in a safe way
+			filename = secure_filename(file.filename)
+			print(filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			column_names, data_part = util.preview_csv(app.config['UPLOAD_FOLDER']+filename)
+			return render_template('upload2.html', column_names=column_names, data_part=data_part)
+	elif request.method == 'GET':
+		return render_template('upload.html')
 
 @app.route('/upload2')
 def upload2():
-    return render_template('upload2.html')
+	return render_template('upload2.html')
 
 @app.route('/verify')
 def verify():
