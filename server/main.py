f14919bcd00866db908709e4c8968624605ddaa3
prasada17 <prasada17@students.ecu.edu>
2019-04-05 01:15:50 -0400

    ..

--- server/main.py
+++ server/main.py
@@ -96,14 +96,3 @@ if __name__ == '__main__':
     ip = '127.0.0.1'
     app.run(host=ip)
 
-@app.route('/api/save', methods=['POST'])
-def process_csv():
-	input_values = request.form
-	result_str = 'First Name:' + request.form['first_name'] + '\n' + \
-				 'Last Name:' + request.form['last_name'] + '\n' + \
-				 'Email:' + request.form['email']
-	# print(result_str)
-	text_file = open(app.config['META_FILE'], "w")
-	text_file.write(result_str)
-	text_file.close()
-	return render_template('success.html')
